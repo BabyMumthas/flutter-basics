342dd1c398a0074db8845813f82ec7f025992b54
BabyMumthas <babymumthas808@gmail.com>
2024-05-01 10:18:58 +0530

    name change

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Peeku Flutter Demo',
+      title: 'Peeku 2 Flutter Demo',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -28,7 +28,8 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 0, 0, 0)),
+        colorScheme:
+            ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 0, 0, 0)),
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Peekkus flutter app'),
